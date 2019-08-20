5124f89c727d033366e4929832af64c636a2acc0
AnnaBorovaya <anna.kviten11@gmail.com>
2019-08-20 09:18:24 +0300

    some

---
+++ precache-manifest.8616917f2fc106798b347c74d8920d63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2700fb4c2593673cb49a51502b2de3",
+    "url": "/to_do-react/index.html"
+  },
+  {
+    "revision": "1b38ce374dbfe105e995",
+    "url": "/to_do-react/static/css/main.5547b0ca.chunk.css"
+  },
+  {
+    "revision": "8a57eb1954f629ca12b7",
+    "url": "/to_do-react/static/js/2.1b2f06e7.chunk.js"
+  },
+  {
+    "revision": "1b38ce374dbfe105e995",
+    "url": "/to_do-react/static/js/main.0b75448c.chunk.js"
+  },
+  {
+    "revision": "62831d831fcf3eb83821",
+    "url": "/to_do-react/static/js/runtime~main.4b45cc04.js"
+  }
+]);
